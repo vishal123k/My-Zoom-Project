d14ab4681515bed525d02cc9e86212799252ae1f
vishal123k <vishalk38916@gamil.com>
2025-04-22 20:45:36 +0530

    auth change

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,7 @@
 import express from "express";
 import { createServer } from "node:http";
-
-import { Server } from "socket.io";
-
 import mongoose from "mongoose";
 import { connectToSocket } from "./controllers/socketManager.js";
-
 import cors from "cors";
 import userRoutes from "./routes/users.routes.js";
 
@@ -13,27 +9,20 @@ const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
-
-app.set("port", (process.env.PORT || 8000))
+app.set("port", process.env.PORT || 8000);
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
-
 app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
-    app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://vishalk389:vishal3890@cluster0.o06uk.mongodb.net/videocall")
-
-    console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`)
-    server.listen(app.get("port"), () => {
-        console.log("LISTENIN ON PORT 8000")
-    });
-
-
-
-}
-
-
-
+  app.set("mongo_user");
+  const connectionDb = await mongoose.connect(
+    "mongodb+srv://vishalk389:vishal3890@cluster0.o06uk.mongodb.net/videocall"
+  );
+  console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`);
+  server.listen(app.get("port"), () => {
+    console.log("LISTENIN ON PORT 8000");
+  });
+};
 start();
